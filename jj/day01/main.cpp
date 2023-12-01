0c00abe4669748f9d0d47c28437bcdbe06d3ca38
Linkiablo <johajansen@googlemail.com>
2023-12-01 15:23:55 +0100

    schicker machen

--- jj/day01/main.cpp
+++ jj/day01/main.cpp
@@ -1,30 +1,30 @@
+#include <algorithm>
 #include <fstream>
 #include <iostream>
 #include <map>
+#include <numeric>
+#include <vector>
 
-const std::map<std::string, int> DIGITS{
+const std::map<std::string_view, int> DIGITS{
     {"one", 1}, {"two", 2},   {"three", 3}, {"four", 4}, {"five", 5},
     {"six", 6}, {"seven", 7}, {"eight", 8}, {"nine", 9}, {"1", 1},
     {"2", 2},   {"3", 3},     {"4", 4},     {"5", 5},    {"6", 6},
     {"7", 7},   {"8", 8},     {"9", 9}};
 
-int part_one(std::string &line) {
-    int res = 0;
-    std::string num;
+int32_t part_one(std::string &line) {
+    std::pair<int32_t, int32_t> res;
 
-    for (auto i = line.begin(); i < line.end(); ++i) {
-        if (std::isdigit(*i)) {
-            num += *i;
-        }
-    }
+    auto first = line.find_first_of("123456789");
+    auto last = line.find_last_of("123456789");
 
-    res = (num.at(0) - 48) * 10 + (num.at(num.length() - 1) - 48);
+    res.first = line.at(first) - 48;
+    res.second = line.at(last) - 48;
 
-    return res;
+    return res.first * 10 + res.second;
 }
 
-int part_two(std::string &line) {
-    int res[2];
+int32_t part_two(std::string &line) {
+    std::pair<int32_t, int32_t> res;
 
     auto first = line.length();
     auto last = 0;
@@ -34,28 +34,28 @@ int part_two(std::string &line) {
         auto last_of = line.rfind(k);
 
         if (first_of != std::string::npos && first_of <= first) {
-            res[0] = v;
+            res.first = v;
             first = first_of;
         }
 
         if (last_of != std::string::npos && last_of >= (unsigned)last) {
-            res[1] = v;
+            res.second = v;
             last = last_of;
         }
     }
 
-    return res[0] * 10 + res[1];
+    return res.first * 10 + res.second;
 }
 
 int main() {
-    int res_one = 0;
-    int res_two = 0;
+    auto res_one = 0;
+    auto res_two = 0;
 
     std::ifstream input_one("input");
 
     for (std::string line; std::getline(input_one, line);) {
-        res_one += part_one(line);
-        res_two += part_two(line);
+	    res_one += part_one(line);
+	    res_two += part_two(line);
     }
 
     std::cout << "part 1: " << res_one << '\n';
